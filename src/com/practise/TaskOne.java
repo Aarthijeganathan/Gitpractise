5c6ba2498b1313d288e9b133c44ab5c18205a85f
Aarthijeganathan <aarthijegan94@gmail.com>
2022-01-02 10:22:25 +0530

    method two updated

--- src/com/practise/TaskOne.java
+++ src/com/practise/TaskOne.java
@@ -5,9 +5,13 @@ public class TaskOne {
 	public void methodOne() {
 		System.out.println("this is method one");
 	}
+		public void methodTwo() {
+		System.out.println("this is method two");
+    }
 	
-public static void main(String[] args) {
+  public static void main(String[] args) {
 	TaskOne to=new TaskOne();
 	to.methodOne();
+	to.methodTwo();
 }
 }
