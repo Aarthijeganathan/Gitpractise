338fb9f49d5b0102bde277463cd5ba1ff8d20e61
Aarthijeganathan <aarthijegan94@gmail.com>
2022-01-02 09:58:17 +0530

    taskone completed

---
+++ src/com/practise/TaskOne.java
@@ -0,0 +1,13 @@
+package com.practise;
+
+public class TaskOne {
+	 
+	public void methodOne() {
+		System.out.println("this is method one");
+	}
+	
+public static void main(String[] args) {
+	TaskOne to=new TaskOne();
+	to.methodOne();
+}
+}
