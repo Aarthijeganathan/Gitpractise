470f3e063f43b9659a335386d273c9cf1a661c26
Aarthijeganathan <aarthijegan94@gmail.com>
2022-01-02 12:07:51 +0530

    aarthi method updated

--- src/com/practise/TaskOne.java
+++ src/com/practise/TaskOne.java
@@ -3,15 +3,19 @@ package com.practise;
 public class TaskOne {
 	 
 	public void methodOne() {
-		System.out.println("this is method one");
+		System.out.println("master this is method one");
 	}
 		public void methodTwo() {
-		System.out.println("this is method two");
+		System.out.println("master this is method two");
     }
-	
+		public void methodThree() {
+			System.out.println("aarthi branch this is method three");
+	    }
+		
   public static void main(String[] args) {
 	TaskOne to=new TaskOne();
 	to.methodOne();
 	to.methodTwo();
+	to.methodThree();
 }
 }
